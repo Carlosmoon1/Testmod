76b4f1bc176e441662699e0b9f92d79857cbadc4
Carlosmoon1 <carlosmoon5888@gmail.com>
2023-03-24 07:21:16 +0100

    Added items

---
+++ src/main/java/net/prayforwaifu/testmod/item/ModItems.java
@@ -0,0 +1,23 @@
+package net.prayforwaifu.testmod.item;
+
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.prayforwaifu.testmod.TestMod;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, TestMod.MOD_ID);
+
+    public static final RegistryObject<Item> SUNSTONE = ITEMS.register("sunstone",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> MOONSTONE = ITEMS.register("moonstone",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+}
