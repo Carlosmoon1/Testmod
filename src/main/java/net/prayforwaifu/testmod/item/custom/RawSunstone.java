f832c165599b90c8d1334182a062f0ce04004c28
Carlosmoon1 <carlosmoon5888@gmail.com>
2023-03-31 16:43:50 +0200

    Gem durability

---
+++ src/main/java/net/prayforwaifu/testmod/item/custom/RawSunstone.java
@@ -0,0 +1,19 @@
+package net.prayforwaifu.testmod.item.custom;
+
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.context.UseOnContext;
+
+public class RawSunstone extends Item {
+    public RawSunstone(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public InteractionResult useOn(UseOnContext useOnContext) {
+       if ()
+
+
+       return super.useOn(useOnContext);
+    }
+}
